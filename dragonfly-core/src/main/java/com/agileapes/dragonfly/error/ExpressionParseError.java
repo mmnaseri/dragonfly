eaaa5aac8e0c08ffdbb5e87e65d5042b27887199
Milad Naseri <m.m.naseri@gmail.com>
2013-10-08 00:13:18 +0330

    Added expression parser using ANTLR

---
+++ dragonfly-core/src/main/java/com/agileapes/dragonfly/error/ExpressionParseError.java
@@ -0,0 +1,12 @@
+package com.agileapes.dragonfly.error;
+
+/**
+ * @author Mohammad Milad Naseri (m.m.naseri@gmail.com)
+ * @since 1.0 (2013/10/7, 22:48)
+ */
+public class ExpressionParseError extends Error {
+
+    public ExpressionParseError(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ dragonfly-core/src/main/java/com/agileapes/dragonfly/ext/impl/parser/AnyTypeFilter.java
@@ -0,0 +1,34 @@
+package com.agileapes.dragonfly.ext.impl.parser;
+
+import com.agileapes.couteau.basics.api.Filter;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Mohammad Milad Naseri (m.m.naseri@gmail.com)
+ * @since 1.0 (2013/10/7, 23:45)
+ */
+public class AnyTypeFilter implements Filter<Class<?>> {
+
+    private final List<Filter<Class<?>>> filters;
+
+    public AnyTypeFilter(Filter<Class<?>>... filters) {
+        this(Arrays.asList(filters));
+    }
+
+    public AnyTypeFilter(List<Filter<Class<?>>> filters) {
+        this.filters = filters;
+    }
+
+
+    @Override
+    public boolean accepts(Class<?> item) {
+        for (Filter<Class<?>> filter : filters) {
+            if (filter.accepts(item)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
